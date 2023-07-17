0a89ce6d430b24ad07346b88930350cef0efdcfe
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-07-17 09:49:58 +0300

    Create type for singe Todo Item

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,4 +1,6 @@
-const placeholderList = [
+import { TodoItemProps } from "./types";
+
+const placeholderList: TodoItemProps[] = [
   { text: "task1", done: false },
   { text: "task2", done: false },
   { text: "task3", done: true },
