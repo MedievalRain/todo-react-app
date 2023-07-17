77406fc35e20eafb69541253fdb16b4b76ec31a1
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-07-17 23:19:12 +0300

    Create props for TodoList. Pass them from root app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import "./App.css";
 import TodoList from "./TodoList";
 import AddItem from "./AddItem";
+import { TodoItemType } from "./types";
+const placeholderList: TodoItemType[] = [
+  { text: "task1", done: false },
+  { text: "task2", done: false },
+  { text: "task3", done: true },
+];
+
 function App() {
   return (
     <>
-      <TodoList />
+      <TodoList items={placeholderList} />
       <AddItem />
     </>
   );
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,16 +1,15 @@
 import { TodoItemType } from "./types";
 import TodoItem from "./TodoItem";
-const placeholderList: TodoItemType[] = [
-  { text: "task1", done: false },
-  { text: "task2", done: false },
-  { text: "task3", done: true },
-];
 
-function TodoList() {
+type TodoListProps = {
+  items: TodoItemType[];
+};
+
+function TodoList({ items }: TodoListProps) {
   return (
     <>
       <ul>
-        {placeholderList.map((todoItem) => (
+        {items.map((todoItem) => (
           <TodoItem item={todoItem} />
         ))}
       </ul>
