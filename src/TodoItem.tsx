39d27027893b73b586657203650788b6eb8b6b91
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-07-18 01:03:08 +0300

    Add y padding to Item

--- src/TodoItem.tsx
+++ src/TodoItem.tsx
@@ -10,7 +10,7 @@ interface TodoItemProps {
 
 function TodoItem({ item, index, toggleDone, deleteItem }: TodoItemProps) {
   return (
-    <li className="flex justify-between px-1">
+    <li className="flex justify-between p-1">
       <div className="flex gap-1">
         <input
           type="checkbox"
