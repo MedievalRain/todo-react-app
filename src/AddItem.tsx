ccd387c9a46c42f288a3ddd2691f51e8cf1d3d83
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-07-17 23:57:14 +0300

    Track AddItem value

--- src/AddItem.tsx
+++ src/AddItem.tsx
@@ -1,12 +1,21 @@
 import { TodoItemType } from "./types";
+import { useState } from "react";
 interface AddItemProps {
   setTodoItems: (arg: TodoItemType[]) => void;
 }
 
 function AddItem({ setTodoItems }: AddItemProps) {
+  const [item, setItem] = useState<string>("ww");
   return (
     <div className="flex flex-col gap-1">
-      <input className="bg-slate-200" type="text" />
+      <input
+        className="bg-slate-200"
+        type="text"
+        value={item}
+        onChange={(event) => {
+          setItem(event.target.value);
+        }}
+      />
       <button className="bg-slate-200">Add</button>
     </div>
   );
